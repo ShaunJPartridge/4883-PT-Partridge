38c3be9a02de9df99e626cfc5d75bbf119ff7434
Shaun Partridge <spartridgeb15@gmail.com>
2021-08-25 15:30:51 -0500

    update

---
+++ Assignments/P10055/source.cpp
@@ -0,0 +1,21 @@
+/**
+* Shaun Partridge
+* 4883 Programming Techniques
+* 8/25/2021
+*/
+#include <iostream>
+
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+    int B = 0, A = 0;
+    
+    while (cin >> A >> B) {
+        
+        // your stuff here
+
+    }
+    return 0;
+}
