b4d5ef9c5c2caab9c05e991293364c17c65f6a75
Shaun Partridge <spartridgeb15@gmail.com>
2021-08-26 17:06:12 -0500

    update

--- Assignments/P10055/source.cpp
+++ Assignments/P10055/source.cpp
@@ -3,21 +3,19 @@
 * 4883 Programming Techniques
 * 8/25/2021
 */
-
 #include <iostream>
-
-#define endl "\n"
+#include <cstdlib>
 
 using namespace std;
-
 int main() {
-    //#ifndef ONLINE_JUDGE;
     //freopen("uDebug.txt","r",stdin);
     //freopen("output.txt","w",stdout);
-    //#endif;
-    int B = 0, A = 0;
+    long long B = 0, A = 0;
     while (cin >> A >> B) {
-            cout << (max(A,B) - min(A,B)) << endl;
+        if(cin.eof()){
+            return 0;
+        }
+        cout << abs(A-B) << endl;
     }
-    return 0;
+    
 }
