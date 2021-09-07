ecd80d6a863690335917e73b31bd57f1efbd05ab
Shaun Partridge <spartridgeb15@gmail.com>
2021-09-07 17:51:05 -0500

    update

--- Assignments/P161/source.cpp
+++ Assignments/P161/source.cpp
@@ -1,51 +1,57 @@
 #include <iostream>
 #include <vector>
-//#include <fstream>
-#include <algorithm>
 #include <iomanip>
 
 using namespace std;
 
-
 int main(){
 
-    //#ifndef ONLINE_JUDGE
-    //freopen("uDebug.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);
-    //#endif
+  //#ifndef ONLINE_JUDGE
+  //freopen("uDebug.txt", "r", stdin);
+  //freopen("output.txt", "w", stdout);
+  //#endif
+  vector<int>scens;
+  string line = "";
+  int val = 0, min = INT32_MAX;
+  int count = 0;
+  bool process = false;
 
-    while(1){
-        
-        int val;
-        vector<int>scens(2);
-        cin >> scens[0] >> scens[1];
-
-        while(cin >> val && val != 0) scens.push_back(val);
-        if(scens.size() == 2 && !scens[0] && !scens[1]) break;
-
-        int N = scens.size();
-        int min = *min_element(scens.begin(),scens.end()) - 5;
+  while(cin >> val){
+    if (val == 0) {
+      count++; 
+      if(count == 2){
+        break;
+      }
+      else{
+        // process vector here
         bool allgreen = false;
-        
-        for(int i = min;i < 18000;++i){
-            int g_count = 0;
-            for(int j = 0;j < N;++j){
-                if(i % (2 * scens[j]) < scens[j] - 5) g_count++;
-                else break;
-            }
-            if(g_count == N){
-                int sec = i % 60; i /= 60;
-                int min = i % 60; i /= 60;
-                int hr = i % 24;
-                cout << setw(2) << setfill('0') << hr << ':' << setw(2) 
-                 << setfill('0') << min << ':' << setw(2) << setfill('0') << sec << endl;
-                allgreen = true;
-                break;
-            }
+        for(int i = min;i <= 18000 ;++i){
+          int g_count = 0;
+          for(int j = 0;j < scens.size();++j){
+            if(i % (2 * scens[j]) < scens[j] - 5) g_count++;
+            else break;
+          }
+          if(g_count == scens.size()){
+            int sec = i % 60; i /= 60;
+            int min = i % 60; i /= 60;
+            int hr = i % 24;
+            cout << setw(2) << setfill('0') << hr << ':' << setw(2) 
+             << setfill('0') << min << ':' << setw(2) << setfill('0') << sec << endl;
+            allgreen = true;
+            break;
+          }
         }
-        if(!allgreen){
-            cout << "Signals fail to synchronise in 5 hours\n";
-        }
+        if(!allgreen){ cout << "Time limit exceeded\n";}
+          //cout << min << endl;
+          scens.clear();
+          min = INT32_MAX;
+      }
     }
-    return 0;
+    else {
+      scens.push_back(val);
+      if(val < min) min = val;
+      count = 0;
+    } 
+  }
+  return 0;
 }
