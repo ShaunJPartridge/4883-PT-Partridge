686e01565b32479f058297440c0670b6e7a28d4e
Shaun Partridge <spartridgeb15@gmail.com>
2021-09-07 22:12:11 -0500

    update

--- Assignments/P10038/source.cpp
+++ Assignments/P10038/source.cpp
@@ -18,18 +18,16 @@ int main(){
     vector<int>nums(N);
     for(int i = 0;i < N;i++){
       cin >> nums[i];
-      //cout << nums[i];
     }
+
     vector<int>res;
     map<int,int>tmap;
     int count = 0;
+    
     for(int i = 0;i < N-1;i++){
       res.push_back(abs(nums[i]-nums[i+1]));
       if(res[i] < N){ count++; tmap.insert({res[i],i}); }
     }
-    //for(int i = 0;i < res.size();i++){
-      //if(res[i] < N && res[i] != res[i+1]) count++;
-    //}
     if (tmap.size() == res.size()) cout << "Jolly\n";
     else cout << "Not jolly\n";
   }
