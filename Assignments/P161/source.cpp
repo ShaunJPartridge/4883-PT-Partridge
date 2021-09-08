26af5fff1f20ce319a776ccbb6e80814c397198a
Shaun Partridge <spartridgeb15@gmail.com>
2021-09-07 21:10:32 -0500

    update

--- Assignments/P161/source.cpp
+++ Assignments/P161/source.cpp
@@ -6,15 +6,13 @@ using namespace std;
 
 int main(){
 
-  //#ifndef ONLINE_JUDGE
-  //freopen("uDebug.txt", "r", stdin);
-  //freopen("output.txt", "w", stdout);
-  //#endif
+  #ifndef ONLINE_JUDGE
+  freopen("uDebug.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  #endif
   vector<int>scens;
-  string line = "";
   int val = 0, min = INT32_MAX;
   int count = 0;
-  bool process = false;
 
   while(cin >> val){
     if (val == 0) {
