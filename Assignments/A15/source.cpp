ad8c50273a38e200bcaee40d026556a76518761f
Shaun Partridge <spartridgeb15@gmail.com>
2021-12-02 17:09:02 -0600

    update

--- Assignments/A15/source.cpp
+++ Assignments/A15/source.cpp
@@ -7,27 +7,27 @@ using namespace std;
 unordered_map<char, char> MC = {{'A','A'},{'E','3'},{'H','H'},{'I','I'},{'J','L'},{'L','J'},{'M','M'},{'O','O'},{'S','2'},{'T','T'},
 {'U','U'},{'V','V'},{'W','W'},{'X','X'},{'Y','Y'},{'Z','5'},{'1','1'},{'2','S'},{'3','E'},{'5','Z'},{'8','8'}};
 
+// for iterative approach
 void determineString(string str){
   bool palindrome = true, mirrored = true;
   
   for(int i = 0;i < str.size()/2;i++){
-    
     if(str[i] != str[str.size()-i-1]){
       palindrome = false;
       break;
     }
   }
 
-  for(int i = 0;i < str.size();i++){
-    
-    if(str[i] != MC[str[str.size()-i-1]]){
+  for(int i = 0;i < str.size();i++){// was str.size()
+    if(str[i] != MC[str[str.size()-i-1]]){// was str.size()-i-1
       mirrored = false;
       break;
     }
   }
 
   cout << str << " -- ";
-  if(palindrome && mirrored){
+  //if(str is a )
+  if(palindrome && mirrored){//temp == str
     cout << "is a mirrored palindrome.\n\n";
   }
   else if(!palindrome && mirrored){cout << "is a mirrored string.\n\n";}
@@ -37,27 +37,40 @@ void determineString(string str){
 
 // for recursive approach
 bool isPalindrome(string str,int start,int end){
-  if(end-start==1|| start == end){
-    return 1;
+  
+  // base case
+  if (end-start==1 || start == end) {// was start >= end
+    // tests if last chars equal each other
+    if(str[start] != str[end])return false;
+    else return true;
   }
-  else{
-    if(str[start]==str[end]){
-      return isPalindrome(str, start+1,end-1);
-    }
-    else return 0;
+ 
+  // return false if mismatch happens
+  if (str[start] != str[end]) {
+    return false;
   }
+ 
+  // move to the next pair
+  return isPalindrome(str, start + 1, end - 1);
 }
+
 // for recursive approach to test if mirrored, needs work
 bool isMirrored(string str,int start,int end){
-  if(end-start==1 || start == end){
-    return 1;
+  
+  // base case
+  if (end-start==1 || start == end) {
+    // tests if last char or chars have a reverse char and equal each other
+    if(str[start] != MC[str[end]])return false;
+    else return true;
   }
-  else{
-    if(str[start] == MC[str[end]]){
-      return isMirrored(str,start+1,end-1);
-    }
-    else return 0;
+ 
+  // return false if mismatch happens
+  if (str[start] != MC[str[end]]) {
+    return false;
   }
+ 
+  // move to the next pair
+  return isMirrored(str, start + 1, end - 1);
 }
 
 // for building strings approach
@@ -72,12 +85,11 @@ string getMirrored(string s){
 int main() {
   
   string line = "";
-
-  freopen("infile", "r", stdin);
-  freopen("outfile", "w", stdout);
   
-  while(getline(cin,line) && !line.empty()){
+  // No difference in time between building strings and iterative approach
 
+  while(getline(cin,line) && !line.empty()){
+    // for recursive approach
     string temp = "",temp2 = "";
 
     // Code for building strings approach
@@ -93,20 +105,21 @@ int main() {
     //else if(line == temp && line != temp2){cout << "is a regular palindrome.\n\n";}
     //else cout << "is not a palindrome.\n\n";
 
-    // Code for iterative approach
-    determineString(line);
 
     // Code for recursive approach, needs work
-    //cout << line << " -- ";
-    //if(isPalindrome(line,0,line.size()-1) && isMirrored(line,0,line.size()-1)){
-    //  cout << "is a mirrored palindrome.\n\n";
-    //}
-    //else if(!isPalindrome(line,0,line.size()-1) && isMirrored(line,0,line.size()-1)){
-      //cout << "is a mirrored string.\n\n";
-    //}
-    //else if(isPalindrome(line,0,line.size()-1) && !isMirrored(line,0,line.size()-1)){cout << "is a regular palindrome.\n\n";}
-    //else cout << "is not a palindrome.\n\n";
+    cout << line << " -- ";
+    if(isPalindrome(line,0,line.size()-1) && isMirrored(line,0,line.size()-1)){
+      cout << "is a mirrored palindrome.\n\n";
+    }
+    else if(!isPalindrome(line,0,line.size()-1) && isMirrored(line,0,line.size()-1)){
+      cout << "is a mirrored string.\n\n";
+    }
+    else if(isPalindrome(line,0,line.size()-1) && !isMirrored(line,0,line.size()-1)){cout << "is a regular palindrome.\n\n";}
+    else cout << "is not a palindrome.\n\n";
 
+    // Code for iterative approach
+    //determineString(line);
+    
   }
 
   return 0;
